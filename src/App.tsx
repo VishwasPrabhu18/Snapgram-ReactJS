2a85f5c6b6a12ed5431e182355eaa16ea2be1636
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-24 17:41:43 +0530

    initial setup is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import SignInForm from "./_auth/forms/SignInForm"
+import SignUpForm from "./_auth/forms/SignUpForm"
+import { Home } from "./_root/pages"
+import AuthLayout from "./_auth/AuthLayout"
+import RootLayout from "./_root/RootLayout"
 
 const App = () => {
   return (
-    <main className='flex h-screen'>App</main>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-in" element={<SignUpForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
