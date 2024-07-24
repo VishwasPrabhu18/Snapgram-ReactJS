6516cf53809ea7720f567060d1db3a895ed2fdfb
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-24 16:07:33 +0530

    react-router-dom is added

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import ReactDom from "react-dom/client";
 import App from './App';
 import "./globals.css";
+import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDom.createRoot(document.getElementById('root')!).render(
-  <App />
+  <Router>
+     <App />
+ </Router>
 );
